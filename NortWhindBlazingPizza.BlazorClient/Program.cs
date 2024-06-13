5c152f5e24a7c4ab9ded8bcb27b2e3571b7776eb
sergio ramos <sergio_ramos@autorefax.com>
2024-06-13 17:33:02 -0600

    u

--- NortWhindBlazingPizza.BlazorClient/Program.cs
+++ NortWhindBlazingPizza.BlazorClient/Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using NorthWind.BlazingPizza.Frontend.BusinessObjects.Option;
+using NorthWind.BlazingPizza.Frontend.RazorViews.Routing;
 using NorthWind.BlazingPizza.Frontend.IoC;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 builder.Services.AddBlazingPizzaServices(
-    blazingPizzaOption =>
+    blazingPizzaOptions =>
         builder.Configuration.GetRequiredSection(BlazingPizzaOptions.SectionKey)
-            .Bind(blazingPizzaOption));
+            .Bind(blazingPizzaOptions));
+
 
 await builder.Build().RunAsync();
--- NorthWind.BlazingPizza.Client/GlobalUsings.cs
+++
@@ -1,4 +0,0 @@
-global using Microsoft.AspNetCore.Components.Web;
-global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-global using NorthWind.BlazingPizza.Client;
-global using NorthWind.BlazingPizza.Frontend.RazorViews.Routing;
